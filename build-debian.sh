e93b556cd8c0f66bab3469d3806c07e8ee2decd2
root <root@CERN.grex>
2020-03-28 22:16:06 +0100

    bug additions and performance reductions

--- build-debian.sh
+++ build-debian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update;apt -y install shtool libtool libtool-bin
+apt update;apt -y install shtool libtool libtool-bin autoconf automake 
 mkdir build-aux m4
 libtoolize --force
 aclocal
